eab0a2351db236de41f58bce7f7bd721ecb2125f
PC-03 <PC-03@DESKTOP-517MJT7>
2025-03-10 12:14:48 +0900

    Cookie Study

--- webTest/src/main/java/basic/cookie/CookieDelete.java
+++ webTest/src/main/java/basic/cookie/CookieDelete.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class CookieAdd
+ * Servlet implementation class CookieDelete
  */
-@WebServlet("/cookieAdd.do")
-public class CookieAdd extends HttpServlet {
+@WebServlet("/cookieDelete.do")
+public class CookieDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -26,30 +26,34 @@ public class CookieAdd extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-	/*
-		Cookie 저장하는 방법
+		/*
+			1. 쿠키 데이터의 삭제는 쿠키의 유지시간을 ‘0’으로 설정하는 방법을 사용한다
+		*/
+		// 예) 쿠키이름이 'gender'인 쿠키 삭제하기
+		Cookie[] cookieArr = request.getCookies();
 		
-		1. Cookie 객체를 생성한다
-		    ‘쿠키이름’과 ‘쿠키값’을 문자열로 지정하여 생성한다.    
-		    형식) Cookie cookie변수 = new Cookie(”쿠키이름”, “ 쿠키값’);    
-		    - ‘쿠키값’으로 한글이 사용될 경우 URLEncode.encode()메서드로 인코딩 후 지정한다.
-		    - 하나의 쿠키는 4KB(4096 byte)까지 저장 가능하다
-		    - 하나의 도메인 당 20개 (총 300개)까지 저장가능하다		
-	*/
-		Cookie nameCookie = new Cookie("name", "유재석");
-		Cookie ageCookie = new Cookie("age", 20 + "");
-		Cookie genderCookie = new Cookie("gender", "Male");
-		
-		response.addCookie(nameCookie);
-		response.addCookie(ageCookie);;
-		response.addCookie(genderCookie);
-		
-		out.println("<html><dead><meta charset='utf-8'><title>Cookie 연습</title></head>");
+		out.println("<html><head><meta charset='utf-8'>"
+				+ "<title>쿠키 삭제 연습</title></head>");
 		out.println("<body>");
 		
-		out.println("<h3>Cookie 데이터가 저장되었습니다</h3>");
+		out.println("<h3>쿠키 삭제하기</h3>");
+		if(cookieArr == null || cookieArr.length == 0) {
+			out.println("<h4>저장된 쿠키가 하나도 없습니다</h4>");
+		} else {
+			// 쿠키 배열에서 해당 쿠키 정보 가져오기
+			for(Cookie cookie : cookieArr) {
+				String name = cookie.getName(); // 쿠키 이름 가져오기
+				if("gender".equals(name)) { 	// 쿠키 이름이 'gender'(삭제할 쿠키이름)을 찾는다
+					// 찾는 쿠키의 '유지시간'을 '0'으로 설정한 후 다시 저장한다
+					cookie.setMaxAge(0);
+					response.addCookie(cookie);
+				}
+			}
+		}
+		out.println("<hr>");
+		
 		out.println("<a href='" + request.getContextPath() 
-			+ "/basic/cookie/cookieTest01.jsp'>시작 문서로 가기 </a>");
+		+ "/basic/cookie/cookieTest01.jsp'>시작 문서로 가기 </a>");
 		
 		out.println("</body></html>");
 	}
--- webTest/src/main/java/basic/cookie/CookieRead.java
+++ webTest/src/main/java/basic/cookie/CookieRead.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class CookieAdd
+ * Servlet implementation class CookieRead
  */
-@WebServlet("/cookieAdd.do")
-public class CookieAdd extends HttpServlet {
+@WebServlet("/cookieRead.do")
+public class CookieRead extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -26,30 +26,33 @@ public class CookieAdd extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-	/*
-		Cookie 저장하는 방법
+		/*
+			## 저장된 Cookie 읽어오기	
+				1. 전체 쿠키 정보를 Request객체를 통해서 가져온다				    
+				    가져온 Cookie정보들은 배열에 저장되어 반환된다	    
+	    			- 형식) Cookie[] 쿠키배열변수 = request객체.getCookies();
+		*/
 		
-		1. Cookie 객체를 생성한다
-		    ‘쿠키이름’과 ‘쿠키값’을 문자열로 지정하여 생성한다.    
-		    형식) Cookie cookie변수 = new Cookie(”쿠키이름”, “ 쿠키값’);    
-		    - ‘쿠키값’으로 한글이 사용될 경우 URLEncode.encode()메서드로 인코딩 후 지정한다.
-		    - 하나의 쿠키는 4KB(4096 byte)까지 저장 가능하다
-		    - 하나의 도메인 당 20개 (총 300개)까지 저장가능하다		
-	*/
-		Cookie nameCookie = new Cookie("name", "유재석");
-		Cookie ageCookie = new Cookie("age", 20 + "");
-		Cookie genderCookie = new Cookie("gender", "Male");
+		Cookie[] cookieArr = request.getCookies();
 		
-		response.addCookie(nameCookie);
-		response.addCookie(ageCookie);;
-		response.addCookie(genderCookie);
-		
-		out.println("<html><dead><meta charset='utf-8'><title>Cookie 연습</title></head>");
+		out.println("<html><head><meta charset='utf-8'><title>Cookie 읽기</title></head>");
 		out.println("<body>");
 		
-		out.println("<h3>Cookie 데이터가 저장되었습니다</h3>");
+		out.println("<h3>저장된 Cookie 확인하기</h3>");
+		if(cookieArr == null || cookieArr.length == 0) {
+			out.println("<h4>저장된 쿠키가 하나도 없습니다</h4>");
+		} else {
+			// 2. 쿠키 배열에서 해당 쿠키 정보를 구해온다
+			for(Cookie cookie : cookieArr) {
+				String name = cookie.getName();		// '쿠키이름' 구하기
+				String value = cookie.getValue();	// '쿠키값' 구하기
+				
+				out.println("쿠키이름 : " + name + "<br>");
+				out.println("쿠키 값 : " + value + "<br><hr>");
+			}
+		}
 		out.println("<a href='" + request.getContextPath() 
-			+ "/basic/cookie/cookieTest01.jsp'>시작 문서로 가기 </a>");
+		+ "/basic/cookie/cookieTest01.jsp'>시작 문서로 가기 </a>");
 		
 		out.println("</body></html>");
 	}
